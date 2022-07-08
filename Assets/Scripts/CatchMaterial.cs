fc6551497ec1f027cb402c36489334052b5bee35
exitshadow <exertingshadows@gmail.com>
2022-07-08 11:42:57 +0200

    added baseMat that stores initialized material to revert later

--- Assets/Scripts/CatchMaterial.cs
+++ Assets/Scripts/CatchMaterial.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class CatchMaterial : MonoBehaviour
 {
     public RaycastHit hit;
+    Material baseMat;
 
-    // Update is called once per frame
+    void Start() {
+        baseMat = GetComponent<Renderer>().material;
+    }
     void Update()
     {
         Color color = Color.white;
-        Debug.DrawRay(transform.position, transform.forward * 20, color, 3);
-
         if (Physics.Raycast(transform.position, transform.forward, out hit, 20)) {
             if (hit.transform.GetComponent<Renderer>() != null) {
                 GetComponent<Renderer>().material = hit.transform.GetComponent<Renderer>().material;
-                color = Color.blue;
+                color = hit.transform.GetComponent<Renderer>().material.color;
             }
+        } else {
+            GetComponent<Renderer>().material = baseMat;
         }
+        Debug.DrawRay(transform.position, transform.forward * 20, color, 3);
     }
 }
