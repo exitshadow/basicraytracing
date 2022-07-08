2f619f58f2f6242f7fc84658c48ad839cd11a96f
exitshadow <exertingshadows@gmail.com>
2022-07-08 10:06:39 +0200

    added color conditions to raytracing tracker

--- Assets/Scripts/CheckTargetVisibility.cs
+++ Assets/Scripts/CheckTargetVisibility.cs
@@ -5,18 +5,30 @@ using UnityEngine;
 public class CheckTargetVisibility : MonoBehaviour
 {
     public Transform target;
+    RaycastHit hit;
+    Color hitColor = Color.red;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 direction = target.position - transform.position;
-        direction = direction.normalized;
-        Debug.DrawRay(transform.position, direction, Color.magenta, 3);
+        direction.Normalize();
+        Debug.DrawRay(transform.position, direction, hitColor, 3);
+
+        if (Physics.Raycast(transform.position, direction, out hit))
+        {
+            if (hit.transform == target)
+            {
+                hitColor = Color.green;
+            } else hitColor = Color.red;
+        }
+
+        Debug.DrawLine(transform.position, target.position, hitColor, 3);
     }
 }
