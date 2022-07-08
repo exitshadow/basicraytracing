87c90b5f1eb61850f8e6a6f26ad2a209438992b0
exitshadow <exertingshadows@gmail.com>
2022-07-08 10:46:36 +0200

    now gameObjects can catch others’ materials when in range, like a disease

---
+++ Assets/Scripts/CatchMaterial.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchMaterial : MonoBehaviour
+{
+    public RaycastHit hit;
+    Color color = Color.white;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.forward * 20, color, 3);
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 20)) {
+            gameObject.GetComponent<Renderer>().material = hit.transform.GetComponent<Renderer>().material;
+            color = Color.blue;
+        }
+    }
+}
